f1be00f2b8b0504ec6e1d8fba062d73bb463cc6a
Farhan Karmali <farhan.karmali@openlms.net>
2023-08-15 22:27:46 +0300

    OWORK-644 Add translations for eu and gl

---
+++ lang/eu/block_myprograms.php
@@ -0,0 +1,46 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * My programs block
+ *
+ * @package    block_myprograms
+ * @copyright  2022 Open LMS (https://www.openlms.net/)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['myprograms:myaddinstance'] = 'Gehitu Nire programen beste bloke bat Panelean';
+$string['myprograms:addinstance'] = 'Gehitu Nire programen beste bloke bat Panelean';
+$string['pluginname'] = 'Nire programak';
+$string['privacy:metadata'] = 'Nire programen blokeak ez du daturik gordetzen bere kabuz.';
+$string['allocationdate'] = 'Esleipen-data';
+$string['allocationend'] = 'Esleipenaren amaiera';
+$string['allocationstart'] = 'Esleipenaren hasiera';
+$string['completiondate'] = 'Osatze-data';
+$string['content'] = "Edukia";
+$string['error:cannotaccessprogram'] = 'Ezin daukazu programa hau atzitzeko baimenik';
+$string['error:coursemisconfigured'] = 'Ikastaroa gaizki konfiguratuta dago';
+$string['filterprograms'] = 'Iragazi nire programak';
+$string['noprograms'] = 'Ez dago erakusteko programa-informaziorik.';
+$string['programdetail'] = 'Programaren xehetasunak';
+$string['programend'] = 'Programaren amaiera';
+$string['programdue'] = 'Programaren iraungitze-data';
+$string['programprogress'] = 'Programaren garapena';
+$string['programstart'] = 'Programaren hasiera';
+$string['programstatus'] = 'Programaren egoera';
+$string['sequencetype'] = 'Osatze mota';
---
+++ lang/gl/block_myprograms.php
@@ -0,0 +1,46 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * My programs block
+ *
+ * @package    block_myprograms
+ * @copyright  2022 Open LMS (https://www.openlms.net/)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['myprograms:myaddinstance'] = 'Engadir un novo bloque dos Meus programas ó Panel';
+$string['myprograms:addinstance'] = 'Engadir un novo bloque dos Meus programas ó Panel';
+$string['pluginname'] = 'Os meus programas';
+$string['privacy:metadata'] = 'O bloque dos Meus programas non almacena datos en si mesmo.';
+$string['allocationdate'] = 'Data de asignación';
+$string['allocationend'] = 'Fin da asignación';
+$string['allocationstart'] = 'Comezo da asignación';
+$string['completiondate'] = 'Data de finalización';
+$string['content'] = "Contido";
+$string['error:cannotaccessprogram'] = 'Non tes acceso a este programa';
+$string['error:coursemisconfigured'] = 'O curso non está ben configurado';
+$string['filterprograms'] = 'Filtrar os meus programas';
+$string['noprograms'] = 'Non hai información que amosar sobre o programa.';
+$string['programdetail'] = 'Detalles do programa';
+$string['programend'] = 'Fin do programa';
+$string['programdue'] = 'Prazo de entrega do programa';
+$string['programprogress'] = 'Progreso do programa';
+$string['programstart'] = 'Comezo do programa';
+$string['programstatus'] = 'Estado do programa';
+$string['sequencetype'] = 'Tipo de finalización';
