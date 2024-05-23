27becf6bffec310710e06857caa8d075f5d65e37
Petr Skoda <petr.skoda@openlms.net>
2024-05-23 16:27:21 +0200

    OWORK-1002 dev: start 3.1.0-dev

--- version.php
+++ version.php
@@ -31,7 +31,7 @@ $plugin->version   = 2024010800;
 $plugin->requires  = 2023100902.00; // 4.3.2
 $plugin->component = 'block_myprograms';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = 'v3.0.0';
+$plugin->release   = 'v3.1.0dev';
 $plugin->supported = [403, 403];
 
 $plugin->dependencies = ['enrol_programs' => 2024010800];
